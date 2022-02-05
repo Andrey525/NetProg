8fc40f3232e5240af58c3b38b2575d73f3d9b239
Andrey525 <ghost5run@yandex.ru>
2022-02-05 20:57:16 +0700

    TCP-Client-Server-Multiproc

---
+++ client_server_TCP_fork/src/client.c
@@ -0,0 +1,53 @@
+#include <arpa/inet.h>
+#include <memory.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  int sock;
+  struct sockaddr_in server_addr;
+  struct hostent *hp;
+  int i, n;
+
+  if (argc < 4) {
+    printf("ENTER ./bin/client hostname port n\n");
+    exit(1);
+  }
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
+
+  memset(&server_addr, 0, sizeof(server_addr));
+
+  server_addr.sin_family = AF_INET;
+  hp = gethostbyname(argv[1]);
+  bcopy(hp->h_addr, &server_addr.sin_addr, hp->h_length);
+  server_addr.sin_port = htons(atoi(argv[2]));
+
+  if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    perror("connect");
+    exit(2);
+  }
+
+  printf("CLIENT: Is ready for sending\n");
+  printf("CLIENT: Sending...\n");
+  n = atoi(argv[3]);
+  for (i = 0; i < n; i++) {
+    send(sock, &n, sizeof(int), 0);
+    printf("CLIENT: Sent %d from %d\n", i + 1, n);
+    sleep(n);
+  }
+
+  printf("CLIENT: Sending complete\n");
+
+  close(sock);
+
+  return 0;
+}
---
+++ client_server_TCP_fork/src/server.c
@@ -0,0 +1,78 @@
+#include <arpa/inet.h>
+#include <memory.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_QUEUE 10
+
+int main() {
+  signal(SIGCHLD,
+         SIG_IGN);  // после завершения процессы не преобразовываются в зомби
+  int sock_client, sock_server;
+  socklen_t length;
+  struct sockaddr_in server_addr, client_addr;
+  int n;
+  int bytes_read;
+  pid_t pid;
+
+  if ((sock_server = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
+
+  memset(&server_addr, 0, sizeof(server_addr));
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = 0;
+
+  if (bind(sock_server, (struct sockaddr*)&server_addr, sizeof(server_addr)) <
+      0) {
+    perror("bind");
+    exit(2);
+  }
+  length = sizeof(server_addr);
+  if (getsockname(sock_server, (struct sockaddr*)&server_addr, &length) < 0) {
+    perror("getsockname");
+    exit(3);
+  }
+  printf("SERVER: port number - %d\n", ntohs(server_addr.sin_port));
+
+  listen(sock_server, MAX_QUEUE);
+
+  while (1) {
+    if ((sock_client = accept(sock_server, (struct sockaddr*)&client_addr,
+                              &length)) < 0) {
+      perror("accept");
+      exit(4);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+      close(sock_server);
+      while (1) {
+        if ((bytes_read = recv(sock_client, &n, sizeof(int), 0)) <= 0) {
+          break;
+        }
+        printf("SERVER: Client address: %s:%d\n",
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        printf("SERVER: Received n = %d\n", n);
+      }
+      close(sock_client);
+      exit(EXIT_SUCCESS);
+    } else if (pid < 0) {
+      perror("fork");
+      exit(5);
+    } else {
+      close(sock_client);
+    }
+  }
+
+  close(sock_server);
+  return 0;
+}
